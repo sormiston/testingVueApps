7b80eb89b5b9fe47569a9a06478f2e93bb6ccc64
SOrmiston <sean.david.ormiston@gmail.com>
2021-03-16 00:25:51 -0400

    jeez

--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -1,14 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
-import ProgressBar from '../ProgressBar.vue'
+import { shallowMount } from '@vue/test-utils';
+import ProgressBar from '../ProgressBar.vue';
 
 describe('ProgressBar.vue', () => {
   test('is hidden on initial render', () => {
-    const wrapper = shallowMount(ProgressBar)
-    expect(wrapper.classes()).toContain('hidden') // #A
-  })
+    const wrapper = shallowMount(ProgressBar);
+    expect(wrapper.classes()).toContain('hidden'); // #A
+  });
 
   test('initializes with 0% width', () => {
+    const wrapper = shallowMount(ProgressBar);
+    expect(wrapper.element.style.width).toBe('0%'); // #A
+  });
+
+  test('displays the bar when start is called', () => {
     const wrapper = shallowMount(ProgressBar)
-    expect(wrapper.element.style.width).toBe('0%') // #A
-  })
-})
+    expect(wrapper.classes()).toContain('hidden')
+    wrapper.vm.start()
+    expect(wrapper.classes()).not.toContain('hidden')
+  });
+});
